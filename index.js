bec68d04eb8985875c129983e4d84de76e1eae3d
EnigmaticaGH <eventhorizonsoundtracks@gmail.com>
2020-05-30 14:21:19 -0400

    change port to 8080

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const _port = 3000;
+const _port = 8080;
 const _app_folder = '../sodacalc/dist/sodacalc';
 
 const app = express();
@@ -15,10 +15,11 @@ app.get('*.*', express.static(_app_folder, {maxAge: '1y'}));
 
 // ---- SERVE APLICATION PATHS ---- //
 app.all('*', function (req, res) {
+  console.log(`Request from ${req.headers.host}`);
   res.status(200).sendFile(`/`, {root: _app_folder});
 });
 
 // ---- START UP THE NODE SERVER  ----
 app.listen(_port, function () {
-  console.log("Node Express server for " + app.get('name') + " listening on http://localhost:" + _port);
+  console.log(`Node Express server for ${app.get('name')} listening on port ${_port}`);
 });
